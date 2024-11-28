364a6befedb5442db45c281c97697a7361bb12a7
Andre Postiga Stone <andre.postiga@pagar.me>
2024-11-27 23:36:49 -0300

    feat: add containerization environment

--- src/UniLx.ApiService/Program.cs
+++ src/UniLx.ApiService/Program.cs
@@ -4,6 +4,8 @@ using UniLx.ApiService.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Configuration.AddEnvironmentVariables("UniLx_");
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(opts =>
 {
---
+++ src/UniLx.Tests/MainTests.cs
@@ -0,0 +1,11 @@
+﻿namespace UniLx.Tests
+{
+    public class MainTests
+    {
+        [Fact(DisplayName = "Sample Test")]
+        public void Test()
+        {
+            Assert.True(true);
+        }
+    }
+}
