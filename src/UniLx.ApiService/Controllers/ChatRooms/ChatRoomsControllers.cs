467ea4fe5818124784be228e2ccc182cb7df1805
Andre Postiga Stone <andre.postiga@pagar.me>
2024-12-23 21:24:42 -0300

    fix: add swagger gen

--- src/UniLx.ApiService/Controllers/ChatRooms/ChatRoomsControllers.cs
+++ src/UniLx.ApiService/Controllers/ChatRooms/ChatRoomsControllers.cs
@@ -36,7 +36,14 @@ namespace UniLx.ApiService.Controllers.ChatRooms
     }
 
     public static class AdminControllerHandlers
-    {   
+    {
+        /// <summary>
+        /// Creates a new chat room for a specified advertisement.
+        /// </summary>
+        /// <param name="request">The request to create a chat room, including advertisement and user details.</param>
+        /// <param name="mediator">The <see cref="IMediator"/> service for handling the command.</param>
+        /// <param name="ct">The <see cref="CancellationToken"/> for task cancellation.</param>
+        /// <returns>An <see cref="IResult"/> indicating success or failure.</returns>
         internal static async Task<IResult> CreateChatRoom([FromBody] CreateChatRoomRequest request,
                 [FromServices] IMediator mediator,
                 CancellationToken ct)
@@ -46,6 +53,14 @@ namespace UniLx.ApiService.Controllers.ChatRooms
             return response!;
         }
 
+        /// <summary>
+        /// Retrieves chat rooms initiated by the current user.
+        /// </summary>
+        /// <param name="context">The HTTP context of the current request.</param>
+        /// <param name="request">The query parameters for retrieving chat rooms.</param>
+        /// <param name="mediator">The <see cref="IMediator"/> service for handling the query.</param>
+        /// <param name="ct">The <see cref="CancellationToken"/> for task cancellation.</param>
+        /// <returns>An <see cref="IResult"/> containing the list of chat rooms.</returns>
         internal static async Task<IResult> GetUserChatRooms(HttpContext context,
                 [AsParameters] GetChatRoomsRequest request,
                 [FromServices] IMediator mediator,
@@ -56,6 +71,14 @@ namespace UniLx.ApiService.Controllers.ChatRooms
             return response!;
         }
 
+        /// <summary>
+        /// Retrieves chat rooms related to the user's advertisements.
+        /// </summary>
+        /// <param name="context">The HTTP context of the current request.</param>
+        /// <param name="request">The query parameters for retrieving chat rooms related to advertisements.</param>
+        /// <param name="mediator">The <see cref="IMediator"/> service for handling the query.</param>
+        /// <param name="ct">The <see cref="CancellationToken"/> for task cancellation.</param>
+        /// <returns>An <see cref="IResult"/> containing the list of chat rooms.</returns>
         internal static async Task<IResult> GetOwnerChatRooms(HttpContext context,
                 [AsParameters] GetChatRoomsRequest request,
                 [FromServices] IMediator mediator,
